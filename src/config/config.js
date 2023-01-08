71c00d4d950d999a76f9a2dc3ba4e2c8864aa401
Strovsk <strovsk@outlook.com>
2023-01-08 02:56:49 -0300

    fix: __dirname import resolve

--- src/config/config.js
+++ src/config/config.js
@@ -1,35 +1,29 @@
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 class Config {
-  static __filename = fileURLToPath(import.meta.url);
-  static __dirname = path.dirname(this.__filename);
-  static configFilePath = path.join(this.__dirname, 'config.json');
-  
   static checkConfigFile() {
     return fs.existsSync(this.configFilePath);
   }
-  
+
   static #createConfigFile() {
     const content = {
-      installPath: this.__dirname,
+      installPath: __dirname,
     };
-    fs.writeFileSync(this.configFilePath, JSON.stringify(content), { encoding: 'utf-8' });
+    fs.writeFileSync(this.configFilePath, JSON.stringify(content), {
+      encoding: 'utf-8',
+    });
   }
-  
+
   static #readConfigFile() {
     if (!this.checkConfigFile()) this.#createConfigFile();
-    
+
     return JSON.parse(fs.readFileSync(this.configFilePath));
   }
-  
+
   static baseurl(location) {
     const config = this.#readConfigFile();
-    return path.join(
-      config.installPath,
-      location,
-    );
+    return path.join(config.installPath, location);
   }
 }
 
