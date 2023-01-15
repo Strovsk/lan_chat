356c9ba08aeac5c1228629cf1b7e61fd23a0d6aa
Strovsk (Thiago) <strovsk@outlook.com>
2023-01-15 20:19:38 -0300

    feat: file config check and get data entry from config file

--- src/config/config.js
+++ src/config/config.js
@@ -2,13 +2,16 @@ import fs from 'fs';
 import path from 'path';
 
 class Config {
+  static configFilePath = path.join(__dirname, 'config.json');
+
   static checkConfigFile() {
     return fs.existsSync(this.configFilePath);
   }
 
   static #createConfigFile() {
     const content = {
-      installPath: __dirname,
+      installPath: path.resolve(__dirname, '..'),
+      logPath: path.resolve(__dirname, '..', 'logs'),
     };
     fs.writeFileSync(this.configFilePath, JSON.stringify(content), {
       encoding: 'utf-8',
@@ -17,13 +20,18 @@ class Config {
 
   static #readConfigFile() {
     if (!this.checkConfigFile()) this.#createConfigFile();
-
     return JSON.parse(fs.readFileSync(this.configFilePath));
   }
 
-  static baseurl(location) {
+  static baseurl(location, keyConfigPath = '') {
     const config = this.#readConfigFile();
+    if (!!keyConfigPath) return path.join(config[keyConfigPath], location);
     return path.join(config.installPath, location);
+  }
+
+  static getConfigData(key) {
+    const config = this.#readConfigFile();
+    return config[key];
   }
 }
 
